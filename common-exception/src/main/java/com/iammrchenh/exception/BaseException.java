cefc238ecfb421987e4b51806893c0244665e890
chen <iammrchen@outlook.com>
2021-01-07 23:14:45 +0800

    init

---
+++ common-exception/src/main/java/com/iammrchenh/exception/BaseException.java
@@ -0,0 +1,4 @@
+package com.iammrchenh.exception;
+
+public class BaseException {
+}
---
+++ common-utils/src/main/java/com/iammrchenh/utils/DateUtils.java
@@ -0,0 +1,32 @@
+package com.iammrchenh.utils;
+
+import com.sun.istack.internal.NotNull;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * 时间工具类，Date和JDK8新增的LocalDate、LocalDateTime 的转换工具类
+ */
+public abstract class DateUtils {
+
+    public static Date toDate(@NotNull LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date toDate(@NotNull LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDate toLocalDate(@NotNull Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public static LocalDateTime toLocalDateTime(@NotNull Date date) {
+        return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+}
